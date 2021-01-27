6837326ba626684443734ad6d4a58c82dcf6d01d
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2021-01-27 09:36:56 +0530

    Installed body parser to the project and sent back the result of operations on 2 numbers like add, sub, mul & div on onClick of submit button

--- calculator.js
+++ calculator.js
@@ -1,10 +1,30 @@
 //jshint esversion:6
 
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req,res){
 	res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req,res){
+
+	var n1 = Number(req.body.Num1);
+	var n2 = Number(req.body.Num2);
+
+	var add = n1+n2;
+	var sub = (n1-n2);
+	var mul = n1*n2;
+	var div = n1/n2;
+
+	res.send("Addition is: " + add + 
+		"<br> Substraction is: " + sub +
+		"<br> Multiplication is: " + mul +
+		"<br> Division is: " + div 
+		);
 });
 
 app.listen(3000, function(){
